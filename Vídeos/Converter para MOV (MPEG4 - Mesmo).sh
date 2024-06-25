f53a0fcda76b5b3fc15f03c43874b5c3709a3dc5
Natanael Barbosa Santos <104917678+natanael-b@users.noreply.github.com>
2024-06-25 08:28:28 -0300

    Fix shcheck issues and notify when errors occurs

--- Vídeos/Converter para MOV (MPEG4 - Mesmo).sh
+++ Vídeos/Converter para MOV (MPEG4 - Mesmo).sh
@@ -1,14 +1,38 @@
-#!/bin/bash
+#!/env/bin/env bash
 
 {
 
-readarray FILENAME <<< "$(echo -e "$NAUTILUS_SCRIPT_SELECTED_FILE_PATHS" | sed -e 's/\r//g')"
+icon="/home/raulcraveiro/.local/share/icons/custom/transform-symbolic.svg"
+
+readarray FILENAME <<< "$(echo -e "${NAUTILUS_SCRIPT_SELECTED_FILE_PATHS}" | sed -e 's/\r//g')"
+
+errors=false
+failed_files=""
+directory=""
 
 for file in "${FILENAME[@]}"; do
-    file=$(echo "$file" | tr -d $'\n')
-    ffmpeg -i "$file" -c:v mpeg4 -map 0 -q:v 0 -codec:a pcm_s16le "${file%.*}-converted.mov"
+    directory=$(dirname "${directory}")
+    file=$(echo "${file}" | tr -d $'\n')
+    ffmpeg -i "${file}" -c:v mpeg4 -map 0 -q:v 0 -codec:a pcm_s16le "${file%.*}-converted.mov" || {
+        errors=true;
+        if [ "${failed_files}" = "" ] ; then
+            failed_files=" ${file}";
+        else
+            failed_files="${failed_files}"$'\n'" ${file}";
+        fi
+    }
 done
 
-notify-send "Deu certo!" "Os arquivos foram convertidos para MOV com sucesso!" --app-name="Transform" --icon="/home/raulcraveiro/.local/share/icons/custom/transform-symbolic.svg"
+[ "${errors}" == "true" ] && {
+    echo "${failed_files}" > "${directory}/log.txt"
+    notify-send "Ocorreram alguns erros"                                                                  \
+                'Alguns arquivos não puderam ser convertidos veja o arquivo log.txt para obter uma lista' \
+                --app-name="Transform" --icon="${icon}"
+    exit 
+}
+
+notify-send 'Deu certo!'                                          \
+            'Os arquivos foram convertidos para MOV com sucesso!' \
+            --app-name="Transform" --icon="${icon}"
 
 }
